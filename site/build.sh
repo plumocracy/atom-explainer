82e50e3a4a61deed520315d4bb185b25bc8aafcb
Plum Hill <evelynplumhill@gmail.com>
2026-04-14 01:02:33 -0400

    Update build script to handle rust install

--- site/build.sh
+++ site/build.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 set -e
 
+# Install Rust if not present
+if ! command -v rustup &>/dev/null; then
+	curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
+fi
+source "$HOME/.cargo/env"
+
 # Install wasm-pack if not present
 if ! command -v wasm-pack &>/dev/null; then
 	curl https://rustwasm.github.io/wasm-pack/installer/init.sh -sSf | sh
